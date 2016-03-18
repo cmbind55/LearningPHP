2db78dc82a847869053506f49b4cd5ec6d6ed089
cmbind55 <cmbind55@live.com>
2016-03-18 11:13:26 -0400

    adding some more php code in hello world

--- hello.php
+++ hello.php
@@ -1,8 +1,24 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+	<head>
+		<title>PHP Test</title>
+	</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?> 
+
+<?php phpinfo(); ?>
+
+<h3>
+	<?php
+	echo $_SERVER['HTTP_USER_AGENT'];
+	?>
+</h3>
+
+<?php
+   foreach ($_SERVER as $key=>$value) 
+   {
+      echo $key."=".$value;
+      echo "<br><br>";
+   }
+?>
+</body>
 </html>
