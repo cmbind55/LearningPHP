c65508fb7f5f6efae1aa286b39a11cda793d3561
cmbind55 <cmbind55@live.com>
2016-03-20 00:15:37 -0400

    adding REST call

--- hello.php
+++ hello.php
@@ -5,54 +5,6 @@
 <body>
 <?php echo '<p>Hello World</p>'; ?> 
 
-<?php 
-function CallAPI($method, $url, $data = false)
-{
-    $curl = curl_init();
-
-    switch ($method)
-    {
-        case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
-
-            if ($data)
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            break;
-        case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
-            break;
-        default:
-            if ($data)
-                $url = sprintf("%s?%s", $url, http_build_query($data));
-    }
-
-    // Optional Authentication:
-    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-    $result = curl_exec($curl);
-
-    curl_close($curl);
-
-    return $result;
-}
-?>
-<!--
-https://api.clashofclans.com/v1/clans?name=awesome%20yankees
-curl -X GET --header "Accept: application/json" 
---header "authorization: Bearer <API token>" 
-"https://api.clashofclans.com/v1/clans?name=awesome%20yankees"
--->
-
-<?php
-	$result = CallAPI("POST", "https://api.clashofclans.com/v1/clans", "name=awesome%20yankees")
-	echo $result;
-?>
-
-<!---
 <?php phpinfo(); ?>
 
 <h3>
@@ -68,7 +20,7 @@ curl -X GET --header "Accept: application/json"
       echo "<br><br>";
    }
 ?>
--->
+
 
 </body>
 </html>
